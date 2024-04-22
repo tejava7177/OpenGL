ac38c32bb475a49a78536f5bcbdf9bbc9fef72d4
tejava7177 <wngmstla@sch.ac.kr>
2024-04-22 23:02:33 +0900

    시험공부 이것저것

--- OpenGlTest/OpenGlTest/pushpop2.cpp
+++ OpenGlTest/OpenGlTest/pushpop2.cpp
@@ -1,3 +1,10 @@
+//
+////  pushpop.cpp
+////  OpenGlTest
+////
+////  Created by 심주흔 on 4/19/24.
+//
+//
 ////
 ////  pushpop.cpp
 ////  OpenGlTest
@@ -27,52 +34,60 @@
 //void display(){
 //    glMatrixMode(GL_MODELVIEW);
 //    glLoadIdentity();
-//    
+//
 //    glClear(GL_COLOR_BUFFER_BIT);
-//    
+//
 //    glColor3f(1.0, 0.0, 0.0);
 //    draw_square();
-//    
+//
 //    glPushMatrix();
-//    
+//
 //    glColor3f(0.0, 1.0, 0.0);
 //    glTranslatef(-0.6, 0.6, 0.0);
 //    draw_square();
-//    
+//
 //    glPopMatrix();
-//    glPushMatrix();
 //    
+// 
+//    
+//    glPushMatrix();
 //    glColor3f(0.0, 0.0, 1.0);
 //    glTranslatef(0.6, 0.6, 0.0);
 //    glRotatef(45, 0.0, 0.0, 1.0);
 //    draw_square();
 //    
+//
 //    glColor3f(1.0, 0.0, 1.0);
 //    glScalef(1.2, 1.2, 1.0);
 //    glTranslatef(-0.7, -0.7, 0.0);
+//    
 //    draw_square();
+//   
+//    
+//    
 //    glPopMatrix();
 //    
-//    
-//    glColor3f(1.0, 1.0, 0.0);
-//    glTranslatef(-0.6, -0.6, 0.0);
-//    glScalef(1.2, 1.2, 1.0);
-//    draw_square();
-//    
-//    
+//
+////
+////    glColor3f(1.0, 1.0, 0.0);
+////    glTranslatef(-0.6, -0.6, 0.0);
+////    glScalef(1.2, 1.2, 1.0);
+////    draw_square();
+//
+//
 //    glFlush();
 //}
 //
 //void init(){
 //    glClearColor(1.0, 1.0, 1.0, 0.0);
 //    glColor3f(1.0, 1.0, 1.0);
-//    
+//
 //    glMatrixMode(GL_PROJECTION);
 //    glLoadIdentity();
 //    gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
-//    
+//
 //    glViewport(0, 0, 400, 400);
-//    
+//
 //}
 //
 //int main(int argc, char ** argv){
--- OpenGlTest/OpenGlTest/카메라이동.cpp
+++ OpenGlTest/OpenGlTest/카메라이동.cpp
@@ -1,59 +1,59 @@
-////
-////  카메라이동.cpp
-////  OpenGlTest
-////
-////  Created by 심주흔 on 2024/04/09.
-////
 //
+//  카메라이동.cpp
+//  OpenGlTest
 //
-//#define GL_SILENCE_DEPRECATION      //버전 오류 해결
+//  Created by 심주흔 on 2024/04/09.
 //
-//#include <iostream>
-//#include <OpenGL/gl.h>
-//#include <OpenGl/glu.h>
-//#include <GLUT/glut.h>
-//
-//void init(void){
-//    glClearColor(0.0,0.0,0.0,0.0);
-//    glShadeModel(GL_FLAT);
-//}
-//
-//void display(void){
-//    glClear(GL_COLOR_BUFFER_BIT);
-//    glColor3f(1.0, 1.0, 1.0);
-//    
-//    glMatrixMode(GL_MODELVIEW);
-//    glLoadIdentity();
-//    
-//    gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-//    
-//    //glRotated(90, 1.0, 0.0, 0.0);
-//    glTranslated(0.0, 0.0, -2.0);
-//    glutWireTeapot(1.0);
-//    
-//    glFlush();
-//}
-//
-//
-//void reshape(int w, int h){
-//    glViewport(0, 0, w, h);
-//    glMatrixMode(GL_PROJECTION);
-//    glLoadIdentity();
-//    glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
-//}
-//
-//int main(int argc, char** argv){
-//    glutInit(&argc, argv);
-//    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-//    glutInitWindowSize(500, 500);
-//    
-//    glutInitWindowPosition(100, 100);
-//    glutCreateWindow("Transformation Teapot");
-//    init();
-//    
-//    glutDisplayFunc(display);
-//    glutReshapeFunc(reshape);
-//    glutMainLoop();
-//    
-//    return 0;
-//}
+
+
+#define GL_SILENCE_DEPRECATION      //버전 오류 해결
+
+#include <iostream>
+#include <OpenGL/gl.h>
+#include <OpenGl/glu.h>
+#include <GLUT/glut.h>
+
+void init(void){
+    glClearColor(0.0,0.0,0.0,0.0);
+    glShadeModel(GL_FLAT);
+}
+
+void display(void){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    
+    gluLookAt(3.0, 2.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    
+    //glRotated(90, 1.0, 0.0, 0.0);
+    glTranslated(0.0, 0.0, -2.0);
+    glutWireTeapot(1.0);
+    
+    glFlush();
+}
+
+
+void reshape(int w, int h){
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
+}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Transformation Teapot");
+    init();
+    
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMainLoop();
+    
+    return 0;
+}
